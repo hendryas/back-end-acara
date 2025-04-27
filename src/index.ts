76556c2584b142dec032ce91ae90f7cd5228c4e2
Hendry Agus Setiawan <hendryas321@gmail.com>
2025-04-27 18:32:49 +0700

    Add validation Auth

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import express from 'express';
 import router from './routes/api';
+import bodyParser from 'body-parser';
 
 const app = express();
+app.use(bodyParser.json());
 
 const PORT = 3000;
 
